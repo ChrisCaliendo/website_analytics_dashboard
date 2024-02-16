c0e9e88980e7a0125c69c51d76526f285697e8ae
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-02-15 20:00:01 -0500

    api stable

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,7 +1,10 @@
-const Page = () => {
+import { analytics } from "@/utils/analytics";
+
+const Page = async () => {
+    const pageview = await analytics.retrieveDays("pageview", 2)
     return (  
         <div>
-            
+            <p className="text-white">hello {JSON.stringify(pageview)}</p>
         </div>
     );
 }
--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,9 +1,16 @@
-
+import { getDate } from '@/utils';
 import { redis } from "@/lib/redis"
+import { parse } from 'date-fns';
+
 
 type AnalyticsArgs = {
     retention?: number
 }
+
+type TrackOptions = {
+    persist?: boolean
+}
+
 export class Analytics {
     private retention: number = 60 * 60 * 24 * 7
 
@@ -11,10 +18,52 @@ export class Analytics {
         if(opts?.retention) this.retention = opts.retention
     }
 
-    async track(namespace: string, event: object = {}) {
-        const key = `analytics::${namespace}`
+    async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+
+        let key = `analytics::${namespace}`
+
+        if(!opts?.persist){
+            key += `::${getDate()}`
+        }
 
         await redis.hincrby(key, JSON.stringify(event), 1)
+        if(!opts?.persist){
+            await redis.expire(key, this.retention)
+        }
+    }
+
+    async retrieve(namespace: string, date: string){
+        const res = await redis.hgetall<Record<string, string>>(`analytics::${namespace}::${date}`)
+        
+        return {
+            date,
+            //uses keys to get associated values
+            events: Object.entries(res ?? []).map(([key, value]) => ({
+                [key]: Number(value)
+            }))
+        }
+    }
+
+    
+    async retrieveDays(namespace: string, numOfDays: number) {
+        //the way this is done allows for the loop to call all data and wait for all of them at the same time
+        type AnalyticsPromise = ReturnType<typeof analytics.retrieve>
+        const promises: AnalyticsPromise[] = []
+        for (let i = 0; i < numOfDays; i++) {
+            const keyDate = getDate(i)
+            const promise = analytics.retrieve(namespace, keyDate)
+            promises.push(promise)
+        }   
+        const fetched = await Promise.all(promises)
+
+        const data = fetched.sort((a, b) => {
+            if(parse(a.date, "MM/dd.yyyy", new Date()) > parse(a.date, "MM/dd.yyyy", new Date())){
+                return 1
+            }else{
+                return -1
+            }
+        })
+        return data;
     }
 }
-export const analytics = new Analytics({retention: 3600})
+export const analytics = new Analytics()
