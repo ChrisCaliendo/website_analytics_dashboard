48ee19e49859f2d2e20fac3672a53bb46b67a2ec
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-02-16 15:24:40 -0500

    kinda almost done

--- src/app/analytics/AnalyticsDashboard.tsx
+++ src/app/analytics/AnalyticsDashboard.tsx
@@ -1,11 +1,52 @@
-import { Card } from "@tremor/react";
+"use client"
+import { analytics } from "@/utils/analytics";
+import { BarChart, Card } from "@tremor/react";
 
-const AnalyticsDashboard = () => {
+interface AnalyticsDashboardProps {
+    avgVisitorsPerDay: string
+    visitorsToday: number
+    timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>
+}
+
+const AnalyticsDashboard = ({ avgVisitorsPerDay, visitorsToday, timeseriesPageviews}: 
+AnalyticsDashboardProps) => {
     return (  
-        <div>
-            <Card className="">
-                <p className="text-tremor-default">hello</p>
-            </Card>
+        <div className="flex gap-6">
+            <div className="grid w-full mx-auto grid-rows-3 grid-cols-2">
+                <Card className="w-full mx-auto max-w-xs rounded-lg col-span-1">
+                    <p className="text-tremor-default text-dark-tremor-content">Avg. Visitors/Day</p>
+                    <p>{avgVisitorsPerDay}</p>
+                    
+                </Card>
+                <Card className="w-full mx-auto max-w-xs rounded-lg col-span-1">
+                    <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
+                    <p>{visitorsToday}</p>
+                    
+                </Card>
+                
+            </div>
+            <Card className="w-full mx-auto max-w-xs rounded-lg col-span-2 row-span-12">
+                    {timeseriesPageviews ? (
+                        <BarChart 
+                            allowDecimals={false}
+                            showAnimation
+                            categories={['Visitors']}     
+                            index="name"
+                            data={timeseriesPageviews.map((day) => ({
+                                name: day.date,
+                                Visitors: day.events.reduce((acc, curr) => {
+                                    return acc + Object.values(curr)[0]!
+                                }, 0),
+                            }))}   
+                            
+                        />
+                        
+                    ) : null}
+                    
+
+                    
+                    
+                </Card>
         </div>
     );
 }
--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,12 +1,36 @@
 import { analytics } from "@/utils/analytics";
 import AnalyticsDashboard from "./AnalyticsDashboard";
+import { getDate } from "@/utils";
+
 
 const Page = async () => {
-    const pageview = await analytics.retrieveDays("pageview", 2)
+    const TRACKING_DAYS = 7
+    const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS)
+    const totalPageviews = pageviews.reduce((acc, curr) => {
+        return (
+            //adds the values of all the days
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0)
+    const avgVisitorsPerDay = (totalPageviews/ TRACKING_DAYS).toFixed(1)
+    const visitorsToday = pageviews.filter((event) => event.date === getDate()).reduce
+    ((acc, curr) => {
+        return(
+            acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+        )
+    }, 0) // default value
+
+
     return (  
-        <div>
-            <div className="text-white">
-                <AnalyticsDashboard />
+        <div className="min-h-screen w-full py-12 flex justify-normal items-center">
+            <div className="relative w-full max-w-6xl mx-auto text-white">
+                <AnalyticsDashboard 
+                    avgVisitorsPerDay={avgVisitorsPerDay}
+                    visitorsToday={visitorsToday}
+                    timeseriesPageviews={pageviews}
+                />
             </div>
         </div>
     );
