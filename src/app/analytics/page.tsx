fb1d945820e840c1d99425c331fe153a576b4f8f
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-02-16 16:30:18 -0500

    adding country data soon

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,7 +5,10 @@ import { getDate } from "@/utils";
 
 const Page = async () => {
     const TRACKING_DAYS = 7
+    const topCountriesMap = new Map<string, number>()
+
     const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS)
+
     const totalPageviews = pageviews.reduce((acc, curr) => {
         return (
             //adds the values of all the days
@@ -15,6 +18,7 @@ const Page = async () => {
         )
     }, 0)
     const avgVisitorsPerDay = (totalPageviews/ TRACKING_DAYS).toFixed(1)
+    
     const visitorsToday = pageviews.filter((event) => event.date === getDate()).reduce
     ((acc, curr) => {
         return(
@@ -22,6 +26,24 @@ const Page = async () => {
         )
     }, 0) // default value
 
+    for (let i = 0; i < pageviews.length; i++) {
+        const day = pageviews[i];
+        if(!day) continue
+        for (let j = 0; j < day.events.length; j++) {
+            const event = day.events[j];
+            if(!event) continue
+            const key = Object.keys(event)[0]!
+            const value = Object.values(event)[0]!
+            const parsedKey = JSON.parse(key)
+            const country = parsedKey?.country
+            if(country){
+                if(topCountriesMap.has(country)){
+                    const prevValue = topCountriesMap.get(country)!
+                    topCountriesMap.set(country, prevValue + value)
+                }
+            }
+        }
+    }
 
     return (  
         <div className="min-h-screen w-full py-12 flex justify-normal items-center">
