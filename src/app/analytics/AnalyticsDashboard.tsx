297b883019c8f38c2b2207f25d007ba4e0b72689
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-03-07 11:21:23 -0500

    finished main functionality

--- src/app/analytics/AnalyticsDashboard.tsx
+++ src/app/analytics/AnalyticsDashboard.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { analytics } from "@/utils/analytics";
 import { BarChart, Card } from "@tremor/react";
+import { ArrowDownRight, ArrowRight, ArrowUpRight } from "lucide-react";
+import ReactCountryFlag from "react-country-flag";
+import Badge from "./Badge";
 
 interface AnalyticsDashboardProps {
     topCountries: [string, number][]
@@ -22,39 +25,48 @@ AnalyticsDashboardProps) => {
                 <Card className="w-full mx-auto max-w-xs rounded-lg col-span-1">
                     <p className="text-tremor-default text-dark-tremor-content">Visitors Today</p>
                     <p>{visitorsToday}</p>
-                    
+                    <Badge percentage={(visitorsToday/Number(avgVisitorsPerDay)-1)*100}/>
                 </Card>
                 
             </div>
 
             <Card className="flex flex-col sm:grid grid-cols-4 gap-6">
                 <h2 className="w-full text-dark-tremor-content-strong text-center sm:left-left font-semibold text-xl">
-
+                    This weeks top visitors:
                 </h2>
+                <div className="col-span-3 flexitems-center justify-between flex-wrap gap-8">
+                    {topCountries?.map(([countryCode, num]) => {
+                        return <div className="flex items-center gap-3 text-dark-tremor-content-strong">
+                            <p className="hidden sm:block text-tremor-content">
+                                {countryCode}
+                            </p>
+                            <ReactCountryFlag 
+                                countryCode={countryCode} 
+                                svg 
+                                className="text-5xl sm:text-3xl"
+                            />
+                            <p className="text-tremor-content sm:text-dark-tremor-content-strong">{num}</p>
+                        </div>
+                    })}
+                </div>
             </Card>
 
             <Card className="w-full mx-auto max-w-xs rounded-lg col-span-2 row-span-12">
-                    {timeseriesPageviews ? (
-                        <BarChart 
-                            allowDecimals={false}
-                            showAnimation
-                            categories={['Visitors']}     
-                            index="name"
-                            data={timeseriesPageviews.map((day) => ({
-                                name: day.date,
-                                Visitors: day.events.reduce((acc, curr) => {
-                                    return acc + Object.values(curr)[0]!
-                                }, 0),
-                            }))}   
-                            
-                        />
-                        
-                    ) : null}
-                    
-
-                    
-                    
-                </Card>
+                {timeseriesPageviews ? (
+                    <BarChart 
+                        allowDecimals={false}
+                        showAnimation
+                        categories={['Visitors']}     
+                        index="name"
+                        data={timeseriesPageviews.map((day) => ({
+                            name: day.date,
+                            Visitors: day.events.reduce((acc, curr) => {
+                                return acc + Object.values(curr)[0]!
+                            }, 0),
+                        }))}   
+                    /> 
+                ) : null}
+            </Card>
         </div>
     );
 }
---
+++ src/app/analytics/Badge.tsx
@@ -0,0 +1,22 @@
+import { ArrowDownRight, ArrowRight, ArrowUpRight } from "lucide-react"
+
+const Badge = ({percentage}: {percentage: number}) => {
+    const isPositive = percentage > 0
+    const isNeutral = percentage === 0
+    const isNegative = percentage < 0
+
+    const positiveClass = "bg-green-200 text-green-800"
+    const negativeClass = "bg-red-200 text-red-800"
+    const neutralClass = "bg-zinc-200 text-zinc-800"
+
+    if(isNaN(percentage)) return null
+    return (
+        <span className={`inline-flex gap-1 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${isPositive ? positiveClass : isNegative ? negativeClass : neutralClass}`}>
+            {isPositive && <ArrowUpRight className="h-3 w-3"/>}
+            {isNegative && <ArrowDownRight className="h-3 w-3"/>}
+            {isNeutral && <ArrowRight className="h-3 w-3"/>}
+            {percentage.toFixed(0)}%
+        </span>
+    )
+}
+export default Badge;
